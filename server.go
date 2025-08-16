698d8383a9475cb0ed0ebf66d1dba155c10139f7
Amrender Singh <amrender9798@gmail.com>
2025-08-16 16:12:58 +0530

    admin login

--- server.go
+++ server.go
@@ -6,13 +6,16 @@ import (
 	"myapp/config"
 	"myapp/database"
 	"myapp/handlers"
+	"myapp/models"
+	"myapp/response"
 	"myapp/routes"
 	"myapp/services"
 	"net/http"
-	
+	"time"
 
-	"github.com/gorilla/mux"
+	"github.com/golang-jwt/jwt/v5"
 	h "github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
@@ -46,6 +49,34 @@ func main() {
 
 
 	r := mux.NewRouter()
+
+	r.HandleFunc("/admin/login",func (w http.ResponseWriter, r *http.Request){
+	    email := r.FormValue("email")
+		password := r.FormValue("password")
+
+		if email != "mydelhiagent@gmail.com" || password != "anygroup"{
+			response.Error(w, http.StatusUnauthorized, "invalid credentials")
+			return
+		}
+		claims := &models.Claims{
+			ID:    "1",
+			Phone: "9873462385",
+			Role:  "admin",
+			RegisteredClaims: jwt.RegisteredClaims{
+				ExpiresAt: jwt.NewNumericDate(time.Now().Add(1 * time.Hour)),
+			},
+		}
+	
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		tokenString,err := token.SignedString([]byte(cfg.JWTSecret))
+		if err != nil {
+			response.Error(w, http.StatusUnauthorized, err.Error())
+			return
+		}
+		response.JSON(w, http.StatusOK, map[string]string{"token": tokenString})
+
+	}).Methods("POST")
+
 	routes.RegisterDealerRoutes(r, dealerHandler,cfg.JWTSecret)
 	routes.RegisterLeadRoutes(r,leadHandler,cfg.JWTSecret)
 	routes.RegisterPropertyRoutes(r,propertyHandler,cfg.JWTSecret)
