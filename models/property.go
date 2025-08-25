1f6299aa6135c1b23d2ae47f98619596b77eaf11
Amrender Singh <amrender9798@gmail.com>
2025-08-25 22:31:36 +0530

    done

--- models/property.go
+++ models/property.go
@@ -1,19 +1,33 @@
 package models
 
-import(
-	 "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type Property struct{
-	ID              primitive.ObjectID      `json:"id,omitempty" bson:"_id,omitempty"`
-	DealerID        primitive.ObjectID      `json:"dealer_id" bson:"dealer_id"`
-	Title           string                  `json:"title" bson:"title"` 
-	Description     string                  `json:"description" bson:"description"`  
-	Address         string                  `json:"address" bson:"address"`
-	Price           float64                 `json:"price" bson:"price"`
-	Photos          []string                 `json:"photos,omitempty" bson:"photos,omitempty"`
-    Videos          []string                 `json:"videos,omitempty" bson:"videos,omitempty"`
-	OwnerName       string                  `json:"owner_name,omitempty" bson:"owner_name,omitempty"`
-	OwnerPhone      string                  `json:"owner_phone,omitempty" bson:"owner_phone,omitempty"`
-	NearestLandmark string                  `json:"nearest_landmark" bson:"nearest_landmark"`
+type Property struct {
+	ID              primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	DealerID        primitive.ObjectID `json:"dealer_id" bson:"dealer_id"`
+	Title           string             `json:"title" bson:"title"`
+	Description     string             `json:"description" bson:"description"`
+	Address         string             `json:"address" bson:"address"`
+	Price           float64            `json:"price" bson:"price"`
+	Photos          []string           `json:"photos,omitempty" bson:"photos,omitempty"`
+	Videos          []string           `json:"videos,omitempty" bson:"videos,omitempty"`
+	OwnerName       string             `json:"owner_name,omitempty" bson:"owner_name,omitempty"`
+	OwnerPhone      string             `json:"owner_phone,omitempty" bson:"owner_phone,omitempty"`
+	NearestLandmark string             `json:"nearest_landmark" bson:"nearest_landmark"`
+	SoldBy          string             `json:"sold_by,omitempty" bson:"sold_by,omitempty"`
+}
+
+type PropertyUpdate struct {
+	Title           *string   `json:"title,omitempty" bson:"title,omitempty"`
+	Address         *string   `json:"address,omitempty" bson:"address,omitempty"`
+	NearestLandmark *string   `json:"nearest_landmark,omitempty" bson:"nearest_landmark,omitempty"`
+	SoldBy          *string   `json:"sold_by,omitempty" bson:"sold_by,omitempty"`
+	Price           *float64  `json:"price,omitempty" bson:"price,omitempty"`
+	Description     *string   `json:"description,omitempty" bson:"description,omitempty"`
+	Photos          *[]string `json:"photos,omitempty" bson:"photos,omitempty"`
+	Videos          *[]string `json:"videos,omitempty" bson:"videos,omitempty"`
+	OwnerName       *string   `json:"owner_name,omitempty" bson:"owner_name,omitempty"`
+	OwnerPhone      *string   `json:"owner_phone,omitempty" bson:"owner_phone,omitempty"`
 }
