611284451145bff084a918206a7ee54474a5a8bc
Amrender Singh <amrender9798@gmail.com>
2025-09-07 13:58:38 +0530

    done

--- services/property.go
+++ services/property.go
@@ -126,7 +126,7 @@ func (s *PropertyService) GetPropertiesByDealer(ctx context.Context, dealerID pr
 		page = 1
 	}
 	if limit < 1 || limit > 100 {
-		limit = 21 // Default limit
+		limit = 12 // Default limit
 	}
 
 	redisKey := fmt.Sprintf("properties_by_dealer:%s:page:%d:limit:%d", dealerID.Hex(), page, limit)
