38e33c525c805a5392327e3e91cc689a4e4fcfc5
Amrender Singh <amrender9798@gmail.com>
2025-09-21 12:44:22 +0530

    done

---
+++ middlewares/optional_auth.go
@@ -0,0 +1,43 @@
+// middlewares/auth.go
+package middlewares
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+
+
+func OptionalJWTAuth(jwtSecret string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			authHeader := r.Header.Get("Authorization")
+			if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+				http.Error(w, "Missing or invalid Authorization header", http.StatusUnauthorized)
+				return
+			}
+
+			tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+			claims := jwt.MapClaims{}
+			token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+				return []byte(jwtSecret), nil
+			})
+			if err != nil || !token.Valid {
+				next.ServeHTTP(w,r)
+				return
+			}
+
+			userID, _ := claims["id"].(string)
+			role, _ := claims["role"].(string)
+
+			ctx := context.WithValue(r.Context(), UserIDKey, userID)
+			ctx = context.WithValue(ctx, UserRoleKey, role)
+
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
